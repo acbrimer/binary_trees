f151ff237750269f092d34f4f159528b2ba77667
Adam <arbimer@mac.com>
2021-10-18 14:35:39 -0500

    Added task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - insert new node as the left-child of another node
+ * @parent: pointer to parent node
+ * @value: value for new node
+ *
+ * Return: pointer to the new node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = binary_tree_node(parent, value);
+
+	if (!newNode)
+		return (NULL);
+	if (newNode->left)
+		newNode->parent->left = newNode;
+	parent->left = newNode;
+	return (newNode);
+}
