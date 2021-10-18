1f8b293df33864c2a413ffdb0819dc8492f69261
Adam <arbimer@mac.com>
2021-10-18 14:41:52 -0500

    Fix for 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,8 +9,11 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = binary_tree_node(parent, value);
+	binary_tree_t *newNode;
 
+	if (!parent)
+		return (NULL);
+	newNode = binary_tree_node(parent, value);
 	if (!newNode)
 		return (NULL);
 	if (parent->left)
