90db2923cd5a6a1b0172a21928c1aca8ad27a820
Adam <arbimer@mac.com>
2021-10-18 21:57:46 -0500

    Added solution for task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes in a binary tree
+ * @tree: node to count from
+ *
+ * Return: number of nodes in the tree or 0 for NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    if (!tree || (!tree->left && !tree->right))
+        return (0);
+    return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
